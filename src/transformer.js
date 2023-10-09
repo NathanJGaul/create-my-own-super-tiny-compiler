30d3babc54af3ab9e33704329e47204418a849cc
Nathan Gaul <nathanjgaul@gmail.com>
2023-10-09 08:33:11 -0500

    Add JSDoc for transformer and traverser

--- src/transformer.js
+++ src/transformer.js
@@ -1,3 +1,42 @@
+/**
+ * A visitor object containing methods to be invoked by the traverser when encountering
+ * specific types of AST nodes during traversal. The methods are responsible for transforming
+ * the original AST nodes and populating a new AST with the transformed nodes.
+ *
+ * @typedef {Object} Visitor
+ *
+ * @property {Object} NumberLiteral - Handles the transformation of NumberLiteral nodes.
+ * @property {Function} NumberLiteral.enter - Invoked when entering a NumberLiteral node.
+ *
+ * @property {Object} StringLiteral - Handles the transformation of StringLiteral nodes.
+ * @property {Function} StringLiteral.enter - Invoked when entering a StringLiteral node.
+ *
+ * @property {Object} CallExpression - Handles the transformation of CallExpression nodes.
+ * @property {Function} CallExpression.enter - Invoked when entering a CallExpression node.
+ */
+
+/**
+ * A function that transforms an Abstract Syntax Tree (AST) based on the visitor methods
+ * provided. It initially creates a new AST structure and populates it by traversing the
+ * original AST and applying the visitor methods for transformation.
+ *
+ * @function
+ *
+ * @param {Object} ast - The original AST to be transformed.
+ *
+ * @returns {Object} A new AST that results from applying the transformations specified
+ * in the visitor methods to the nodes of the original AST.
+ *
+ * @example
+ * const originalAst = {
+ *   type: 'Program',
+ *   body: [ ... ]
+ * };
+ *
+ * const transformedAst = transformer(originalAst);
+ *
+ * @see {@link Visitor} for the structure of the visitor object and its methods.
+ */
 const traverser = require("./traverser");
 
 const visitor = {
--- src/traverser.js
+++ src/traverser.js
@@ -1,3 +1,39 @@
+/**
+ * Traverses the Abstract Syntax Tree (AST), visiting each node and applying the methods
+ * specified in the provided visitor object. The traverser function is essential for operations
+ * like transforming and code generation from the AST.
+ *
+ * @param {Object} ast - The Abstract Syntax Tree that needs to be traversed. The structure of the
+ * AST should align with the expected node types and tree structure for proper traversal.
+ *
+ * @param {Object} visitor - An object containing methods corresponding to the type of AST nodes.
+ * Each key in the object should be a node type (e.g., "Program", "CallExpression"), and the value
+ * should be another object containing an 'enter' and/or 'exit' method to be called when entering and/or
+ * exiting the respective node during traversal.
+ *
+ * @example
+ * const ast = {
+ *   type: 'Program',
+ *   body: [ ... ]
+ * };
+ *
+ * const visitor = {
+ *   Program: {
+ *     enter(node, parent) {
+ *       console.log('Entering Program node');
+ *     },
+ *     exit(node, parent) {
+ *       console.log('Exiting Program node');
+ *     }
+ *   },
+ *   // ... other node type handlers
+ * };
+ *
+ * traverser(ast, visitor);  // Calls the enter and exit methods as it traverses the AST
+ *
+ * @throws {TypeError} Throws an error if it encounters an unsupported node type during traversal,
+ * indicating that there's no case handling for that specific node type in the traverser function.
+ */
 const traverser = (ast, visitor) => {
   const traverseArray = (array, parent) => {
     array.forEach((child) => {
